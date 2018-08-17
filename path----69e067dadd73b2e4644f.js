9f45d622fe59529f5cdf2b269363ef6582d71da6
Saatvik Arya <aryasaatvik@gmail.com>
2018-08-17 08:47:56 +0530

    Revert "Updates"
    This reverts commit e63c9a65a7a3b2c731839ed51d36819e2549fde6.

--- path----69e067dadd73b2e4644f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Saatvik Arya"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----69e067dadd73b2e4644f.js.map
